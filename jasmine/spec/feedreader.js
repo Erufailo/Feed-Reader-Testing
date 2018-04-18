790b70e28b10ba0bf3076a1a15496664f2bd3204
erufailo <jrin2113@gmail.com>
2018-04-18 13:32:09 +0300

    Feat:(#8) Adds URL test in allFeeds object

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,6 +31,12 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+        it('should have URLs defined', function() {
+            for (const feed of allFeeds) {
+                expect(feed.url).toBeDefined();
+                expect(feed.url).not.toBe("");
+            }
+        });
 
 
         /* TODO: Write a test that loops through each feed
