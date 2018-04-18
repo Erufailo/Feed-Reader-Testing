b1e99b33de865a392f3eeeaebfe50ddc619b266f
erufailo <jrin2113@gmail.com>
2018-04-18 14:12:21 +0300

    Feat:(#13) Adds test suite with name 'Initial Entries'

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -71,7 +71,7 @@ $(function () {
          * should have two expectations: does the menu display when
          * clicked and does it hide when clicked again.
          */
-        it('should change visibility when the menu icon is clicked', function () {            
+        it('should change visibility when the menu icon is clicked', function () {
             document.querySelector(".icon-list").click();
             expect(document.querySelector("body").classList.contains("menu-hidden")).toBe(false);
             document.querySelector(".icon-list").click();
@@ -81,18 +81,22 @@ $(function () {
     });
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function () {
 
-    /* TODO: Write a test that ensures when the loadFeed
-     * function is called and completes its work, there is at least
-     * a single .entry element within the .feed container.
-     * Remember, loadFeed() is asynchronous so this test will require
-     * the use of Jasmine's beforeEach and asynchronous done() function.
-     */
+        /* TODO: Write a test that ensures when the loadFeed
+         * function is called and completes its work, there is at least
+         * a single .entry element within the .feed container.
+         * Remember, loadFeed() is asynchronous so this test will require
+         * the use of Jasmine's beforeEach and asynchronous done() function.
+         */
+        
 
+    });
     /* TODO: Write a new test suite named "New Feed Selection" */
 
     /* TODO: Write a test that ensures when a new feed is loaded
      * by the loadFeed function that the content actually changes.
      * Remember, loadFeed() is asynchronous.
      */
+
 }());
